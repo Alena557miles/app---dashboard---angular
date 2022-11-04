8f5948931fa772739e2f6750e695a6c16cc0e47f
Alena557miles <557miles@gmail.com>
2022-11-04 14:49:06 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardPageComponent } from './board-page/board-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 
-const routes: Routes = [];
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+
+const routes: Routes = [
+  {
+    path:'', component: MainLayoutComponent, children:[
+      { path:'', redirectTo: '/', pathMatch: 'full'},
+      { path:'', component: DashboardPageComponent},
+      { path:'board/:id', component: BoardPageComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+
+import { BoardItemComponent } from './shared/components/board-item/board-item.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { BoardPageComponent } from './board-page/board-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    BoardItemComponent,
+    DashboardPageComponent,
+    BoardPageComponent
   ],
   imports: [
     BrowserModule,
