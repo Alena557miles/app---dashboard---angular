7a2cb02feacfbeef2c093088a2128cd0a35ce589
Alena557miles <557miles@gmail.com>
2022-11-08 19:15:35 +0200

    create-task component

--- src/app/create-task/create-task.component.ts
+++ src/app/create-task/create-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Task } from '../shared/interfaces';
 
 @Component({
   selector: 'app-create-task',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTaskComponent implements OnInit {
 
+  form: FormGroup;
+  
+  get title(){
+    return this.form.controls.title as FormControl
+  }
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl<string>('',[
+        Validators.required,
+        Validators.minLength(2)
+      ])
+    })
   }
 
+  submit(){
+    if (this.form.invalid){
+      return
+    }
+    console.log(this.form.value.title)
+    const board: Task = {
+      name: this.form.value.title
+    }
+  }
 }
